32b132bb5b5c308f25c1af8d19f15897b35d8c00
Johnny Carreiro <johnny@johnnycarreiro.com>
2023-05-31 15:50:20 -0300

    feat(comp): create default button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import { Button } from '@/components/@core/Button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-24 px-24">
+    <main className="flex min-h-screen flex-col items-center justify-between px-24 py-24">
       <h1>Hello World</h1>
+      <Button
+        className={'rounded-full px-4'}
+        isLoading={false}
+        variant={'secondary'}
+        disabled={false}
+        size={'sm'}
+      >
+        Botão
+      </Button>
     </main>
   )
 }
